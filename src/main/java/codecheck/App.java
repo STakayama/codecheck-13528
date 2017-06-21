f8eeaed1122c01f7bc47ece0c1e199dc5c4704dd
STakayama <g1520524@is.ocha.ac.jp>
2017-06-21 13:58:50 +0900

    Update App.java

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,25 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+		char[] input=new char[1000];
+		char[] input=new char2[1000];
+		Arrays.fill(char,"0");
+		int times=String.valueOf(args[0]);
+		char[0]="1";
+		System.out.println(char[0]);
+		for (int i = 0;i < times; i++) {//入力された数だけ繰り返す
+			
+			for(int j=i;char[j]!=0;j++) {//配列の長さ分だけ
+			int j=i;
+			char check=char[i];
+			count=0;
+			while(char[j]==check){
+				count++;
+			}
+			System.out.print(count);
+			System.out.print(check);
+			}
+
 		}
 	}
 }
